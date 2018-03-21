3a9a48dc3b6793c842a404d9fa85d06f4c3e1cdf
ashan <madu2000official@gmai.com>
2018-03-21 20:13:00 +0530

    add routing and home and header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
-import { ModalModule, BsDropdownModule, TypeaheadModule, TooltipModule, PopoverModule } from 'ngx-bootstrap';
+// import { CarouselModule, CollapseModule, ModalModule, BsDropdownModule, TypeaheadModule, TooltipModule, PopoverModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { LoginComponentComponent } from './online-shooping-components/common-component/login-component/login-component.component';
 import { HomeComponent } from './online-shooping-components/home-component/home/home.component';
+import { HeaderComponent } from './online-shooping-components/common-component/header-bar/header/header.component';
 
+// Routing Module
+import { AppRoutingModule } from './ng-modules/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponentComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    ModalModule.forRoot(),
-    BsDropdownModule.forRoot(),
-    TypeaheadModule.forRoot(),
-    TooltipModule.forRoot(),
-    PopoverModule.forRoot()
+    AppRoutingModule,
+    FormsModule
+    // CarouselModule.forRoot(),
+    // CollapseModule.forRoot(),
+    // ModalModule.forRoot(),
+    // BsDropdownModule.forRoot(),
+    // TypeaheadModule.forRoot(),
+    // TooltipModule.forRoot(),
+    // PopoverModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
