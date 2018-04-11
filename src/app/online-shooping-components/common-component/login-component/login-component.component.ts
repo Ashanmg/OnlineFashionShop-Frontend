f56aa4c359e765ecfdb8d119ad99700981785e98
ashan <madu2000official@gmai.com>
2018-04-11 23:13:22 +0530

    service fixes

--- src/app/online-shooping-components/common-component/login-component/login-component.component.ts
+++ src/app/online-shooping-components/common-component/login-component/login-component.component.ts
@@ -1,3 +1,4 @@
+import { UserService } from './../../../serivces/user.service';
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationStart } from '@angular/router';
 import { NgForm } from '@angular/forms';
@@ -14,7 +15,8 @@ export class LoginComponentComponent implements OnInit {
   _user :UserLogin;
 
   constructor(private router: Router,
-    private toastr: ToastrService) 
+    private toastr: ToastrService,
+    private userService : UserService) 
     { }
 
   ngOnInit() {
@@ -24,7 +26,19 @@ export class LoginComponentComponent implements OnInit {
 
   onSubmitLogin(login  : NgForm) {
     if(login.valid) {
-      this.toastr.success("loaded successfully");
+      let user = {Id: 1, Username:'ashanmg9@gmail.com', Password:'zaq1xsw2!@#$', RememberMe:false}
+      this.userService.testing().subscribe(
+        (data) => {
+          if(data) {
+            this.toastr.success("loggin successfully");
+          }else {
+            this.toastr.error("login failed");
+          }
+        }, (err) => {
+          console.log(err);
+          this.toastr.error("login failed due to connection failiure");
+        }
+      );
     } else {
       this.toastr.error(" please fill the required fields");
       console.log("logged");
--- src/app/serivces/user.service.ts
+++ src/app/serivces/user.service.ts
@@ -1,15 +1,23 @@
+import { environment } from './../../environments/environment';
 import { LocalStorageService } from 'angular-2-local-storage';
 import { Injectable } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class UserService {
+    path : 'http://localhost:1862/api/user/login';
+    loggedIn : false;
     /**
      */
-    constructor(private localStorageService : LocalStorageService) {
+    constructor(private localStorageService : LocalStorageService,
+                private router : Router,
+                private http : HttpClient,
+                private route : ActivatedRoute) {
     }
 
     login(userdetails) {
-        // login post call here
+        return this.http.post(this.path , JSON.stringify(userdetails)).map(response => <any>(<Response>response).json());
     }
     
     saveToken(token) {
@@ -18,6 +26,11 @@ export class UserService {
 
     saveUserDate(userData) {
         // save user details
+    }
+
+    testing() {
+        let path = 'user/test'
+        return this.http.get(environment.apiUrl + path);
     }
     
 }
