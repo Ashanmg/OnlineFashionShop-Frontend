dbe3a7cdef3cede8bbd2601c8323efe25a381cf2
ashan <madu2000official@gmai.com>
2018-04-26 19:59:35 +0530

    product service-product type

---
+++ src/app/serivces/product.service.ts
@@ -0,0 +1,18 @@
+import { environment } from './../../environments/environment';
+import { Injectable } from '@angular/core';
+import { LocalStorageService } from 'angular-2-local-storage';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class ProductService {
+    /**
+     */
+    constructor(private localStorageService: LocalStorageService,
+                private http: HttpClient) {
+    }
+
+    getCategoryDetails() {
+        return this.http.get(environment.apiUrl + 'product/category');
+    }
+
+}
