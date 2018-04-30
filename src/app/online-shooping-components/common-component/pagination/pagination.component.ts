be800307a7af5cbf9317f7a464da9899ca0242ca
ashan <madu2000official@gmai.com>
2018-04-30 22:07:35 +0530

    pagination

---
+++ src/app/online-shooping-components/common-component/pagination/pagination.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.css']
+})
+export class PaginationComponent implements OnInit {
+
+  public page = 3;
+  public pagesCount = 6;
+  public totalCount = 22;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  range(): Array<any> {
+      if (!this.pagesCount) { return []; }
+      const step = 2;
+      const doubleStep = step * 2;
+      const start = Math.max(0, this.page - step);
+      let end = start + 1 + doubleStep;
+      if (end > this.pagesCount) { end = this.pagesCount; }
+
+      const ret = [];
+      for (let i = start; i !== end; ++i) {
+          ret.push(i);
+      }
+
+      return ret;
+  }
+
+  pagePlus(count: number): number {
+      return + this.page + count;
+  }
+
+  search(i: any): void {
+      this.page = i;
+  }
+
+}
