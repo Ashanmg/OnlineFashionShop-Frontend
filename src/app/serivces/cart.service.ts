4a59240de29643ca61716cfddc734d92552682e9
ashan <madu2000official@gmai.com>
2018-06-18 23:04:00 +0530

    lint issues

--- src/app/serivces/cart.service.ts
+++ src/app/serivces/cart.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from "@angular/core";
-import { LocalStorageService } from "angular-2-local-storage";
-import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { LocalStorageService } from 'angular-2-local-storage';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ShoppingCartService {
@@ -10,7 +10,6 @@ export class ShoppingCartService {
      */
     constructor(private http: HttpClient,
                 private localStorageService: LocalStorageService) {
-        
     }
 
     getCartDetailOnLoggin() {
@@ -18,7 +17,6 @@ export class ShoppingCartService {
     }
 
     addToCartDetails() {
-
     }
 
 }
